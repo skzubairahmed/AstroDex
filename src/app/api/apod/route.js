2ddf48f1d62984afb9ea4d36e745ce0a61a49ed1
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-08-30 02:31:29 +0530

    Added caching to apod api.

--- src/app/api/apod/route.js
+++ src/app/api/apod/route.js
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import { unstable_cache } from "next/cache";
 
 let uri = process.env.MONGODB_URI;
 
@@ -8,13 +9,28 @@ let clientPromise;
 client = new MongoClient(uri);
 clientPromise = client.connect();
 
+const getApodData = unstable_cache(
+    async () => {
+            try{
+                client = await clientPromise;
+                const db = client.db("astrodex");
+                const apod = db.collection("apod");
+
+                let data = await apod.findOne({}, {sort:{_id: -1}});
+                return data
+            }catch(e){
+                return e.message
+            }
+    },
+    ["apod-cache"],
+    {revalidate: 3600}
+);
+
 export async function GET(){
     try{
-        client = await clientPromise;
-        const db = client.db("astrodex");
-        const apod = db.collection("apod");
-
-        let data = await apod.findOne({}, {sort:{_id: -1}});
+        let res = await getApodData();
+        let data = res;
+        console.log(data);
         return Response.json(data);
     }catch(e){
         return Response.json({error:e.message}, {status:500});
--- src/app/nasa-apod/page.js
+++ src/app/nasa-apod/page.js
@@ -1,15 +1,76 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
 import Navbar from "../Components/Navbar";
 import SideBar from "../Components/SideBar";
 
 export default function NasaApod() {
+  let [apod, setApod] = useState(null);
+  let [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getApod(){
+      try{
+        let res = await fetch("/api/apod");
+        let data = await res.json();
+        setApod(data);
+        setLoading(false);
+      }catch(e){
+        setApod(null);
+      }
+    }
+
+    getApod();
+  }, [])
+
+  if(loading){
+    return(
+      <div className="bg-bg-primary m-0 p-0 flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+        <p className="text-text-primary text-xl font-mono animate-pulse">
+          FETCHING DATA...
+        </p>
+      </div>
+    )
+  }
   return (
     <>
       <div className="p-0 m-0 w-screen flex flex-col h-screen bg-bg-primary overflow-hidden">
         <Navbar />
         <div className="flex md:flex-row flex-1 overflow-hidden">
           <SideBar />
-          <main className="w-full h-full flex-col p-2 overflow-y-auto">
-            
+          <main className="w-full flex-1 min-h-1 flex flex-col p-4 overflow-y-auto pb-24">
+            <div className="flex w-full flex-col gap-3">
+              <div className="w-full h-fit border-1 border-satellite/20 rounded-lg p-4 flex flex-col gap-3">
+                <p className="text-text-primary font-bold text-lg text-wrap">
+                  🖼️ NASA Astronomy Picture Of the Day: {apod.title}
+                </p>
+                <div className="w-full h-fit flex justify-center">
+                  <div className="w-fit h-fit border-2 border-satellite/20 hover:border-satellite transition-color rounded-lg">
+                    <Link href={apod.url} target="_blank" rel="noopener noreferrer">
+                      <img 
+                      src={apod.url}
+                      className="rounded-lg"
+                      />
+                    </Link>
+                  </div>
+                </div>
+                <div className="w-full h-fit border-1 border-satellite/20 rounded-xl p-3">
+                  <p className="text-text-primary">
+                    <b>Date:</b> {apod.date}
+                  </p>
+
+                  <p className="text-text-primary">
+                    <b>Copyright:</b> {apod.copyright}
+                  </p>
+                  <br/>
+                  <p className="text-text-primary">
+                    <b>Explanaition:</b> <br/>{apod.explaination}
+                  </p>
+                </div>
+              </div>
+            </div>
           </main>
         </div>
       </div>
