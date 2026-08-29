ebe4460f6e8aaae5b047c927f7e6bb695e4deed5
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-08-30 00:59:40 +0530

    Added apod preview, added routes to propagate separate apod store, added route to fetch apod.

--- src/app/api/apod/route.js
+++ src/app/api/apod/route.js
@@ -9,10 +9,14 @@ client = new MongoClient(uri);
 clientPromise = client.connect();
 
 export async function GET(){
-    client = await clientPromise;
-    const db = client.db("astrodex");
-    const apod = db.collection("apod");
+    try{
+        client = await clientPromise;
+        const db = client.db("astrodex");
+        const apod = db.collection("apod");
 
-    let data = await apod.findOne({}, {sort:{_id: -1}});
-    return Response.json(data);
+        let data = await apod.findOne({}, {sort:{_id: -1}});
+        return Response.json(data);
+    }catch(e){
+        return Response.json({error:e.message}, {status:500});
+    }
 }
